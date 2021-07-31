2d99b0bb6bacd81b5f97ad03767bdeaa74cb56e0
JuLong <julong1988@naver.com>
2021-07-31 18:43:44 +0900

    fix: setupFiles추가

--- src/config/jest.config.js
+++ src/config/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
       '<rootDir>/jest/fileMock.js',
     '\\.(css|less|styl)$': `<rootDir>../node_modules/identity-obj-proxy`,
   },
+  setupFiles: ['<rootDir>/jest/setupFiles.js'],
   globals: {
     'ts-jest': {
       tsconfig: path.resolve(__dirname, '../tsconfig.json'),
