88ab56931ae3d187f4666479a5c54fb9dc6b1819
JuLong <julong1988@naver.com>
2021-07-31 21:30:39 +0900

    fix: ts-jest => babel-jest

--- src/config/jest.config.js
+++ src/config/jest.config.js
@@ -1,24 +1,17 @@
 const path = require('path');
-// const alias = require('./alias');
 
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jsdom',
   roots: ['<rootDir>', path.resolve('./src/')],
   transform: {
-    '^.+\\.(js|jsx|ts|tsx|mjs)$': `ts-jest`,
+    '^.+\\.(js|jsx|ts|tsx|mjs)$': ['babel-jest', {
+      configFile: path.resolve(__dirname, 'babel.config.js')
+    }],
   },
   moduleNameMapper: {
-    // ...alias.default,
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/jest/fileMock.js',
     '\\.(css|less|styl)$': `<rootDir>../node_modules/identity-obj-proxy`,
   },
   setupFiles: ['<rootDir>/jest/setupFiles.js'],
-  globals: {
-    'ts-jest': {
-      tsconfig: path.resolve(__dirname, '../tsconfig.json'),
-      useESM: true,
-    },
-  },
 };
